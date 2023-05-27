5f7890a49ba51c596d14c75912a9830eb5735cf5
backdaniel <40582032+backdaniel@users.noreply.github.com>
2023-05-26 21:25:31 -0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -98,34 +98,20 @@ end
 -- MAPGEN
 
 minetest.register_ore({
-  ore_type       = "scatter",
-  ore            = "basalt_fertilizer:basalt",
-  wherein        = "default:stone",
-  clust_scarcity = 8 * 8 * 8,
-  clust_num_ores = 9,
-  clust_size     = 3,
-  y_max          = 31000,
-  y_min          = 1025,
-})
-
-minetest.register_ore({
-  ore_type       = "scatter",
-  ore            = "basalt_fertilizer:basalt",
-  wherein        = "default:stone",
-  clust_scarcity = 8 * 8 * 8,
-  clust_num_ores = 8,
-  clust_size     = 3,
-  y_max          = 64,
-  y_min          = -127,
-})
-
-minetest.register_ore({
-  ore_type       = "scatter",
-  ore            = "basalt_fertilizer:basalt",
-  wherein        = "default:stone",
-  clust_scarcity = 12 * 12 * 12,
-  clust_num_ores = 30,
-  clust_size     = 5,
-  y_max          = -128,
-  y_min          = -31000,
+  ore_type        = "blob",
+  ore             = "basalt_fertilizer:basalt",
+  wherein         = {"default:stone"},
+  clust_scarcity  = 16 * 16 * 16,
+  clust_size      = 14,
+  y_max           = 31000,
+  y_min           = -31000,
+  noise_threshold = 0.0,
+  noise_params    = {
+    offset = 0.5,
+    scale = 0.2,
+    spread = {x = 5, y = 5, z = 5},
+    seed = 111,
+    octaves = 1,
+    persist = 0.0
+  },
 })
