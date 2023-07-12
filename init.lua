2dbd317daf877356c12f3fdc028ec6a1d73feb5e
backdaniel <40582032+backdaniel@users.noreply.github.com>
2023-07-12 01:45:13 -0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -144,20 +144,21 @@ end
 -- MAPGEN
 
 minetest.register_ore({
-  ore_type        = "stratum",
-  ore             = "basalt_fertilizer:basalt",
-  wherein         = {"default:stone"},
-  clust_scarcity  = 18 * 18 * 18,
-  clust_size      = 8,
-  y_max           = 100,
-  y_min           = -31000,
-  noise_threshold = 0.0,
-  noise_params    = {
-    offset = 0.5,
-    scale = 0.2,
-    spread = {x = 5, y = 5, z = 5},
-    seed = 111,
-    octaves = 1,
-    persist = 0.0
-  },
+    ore_type         = "stratum",
+    ore              = "basalt_fertilizer:basalt",
+    wherein          = {"default:stone"},
+    clust_scarcity   = 20 * 20 * 20,
+    clust_size       = 8,
+    y_max            = 64,
+    y_min            = -31000,
+    noise_threshold  = 0.5,
+    noise_params     = {
+        offset  = 0,
+        scale   = 1,
+        spread  = {x = 100, y = 100, z = 100},
+        seed    = 23,
+        octaves = 3,
+        persist = 0.7
+    },
+    stratum_thickness = 10,
 })
